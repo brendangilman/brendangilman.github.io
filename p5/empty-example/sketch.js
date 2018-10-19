d7ad6e3c0e9014321f2eaa1e30d93bc6f0a7a235
Brendan <brendangilman@yahoo.com>
2018-10-18 18:23:20 -0700

    0.2.3
    Added Snake Game :)

--- p5/empty-example/sketch.js
+++ p5/empty-example/sketch.js
@@ -1,14 +1,50 @@
+var s;
+var scl = 20;
+
+var food;
+
 function setup() {
-  // put setup code here
-  createCanvas(480, 480);
+  createCanvas(600, 600);
+  s = new Snake();
+  frameRate(10);
+  pickLocation();
+
+}
+
+function pickLocation() {
+  var cols = floor(width/scl);
+  var rows = floor(height/scl);
+  food = createVector(floor(random(cols)), floor(random(rows)));
+  food.mult(scl);
+}
+
+function mousePressed() {
+  s.total++;
 }
 
 function draw() {
-  // put drawing code here
-  if (mouseIsPressed) {
-  fill(0);
-  } else {
-  fill(255);
+  background(51);
+
+  if (s.eat(food)) {
+    pickLocation();
   }
-  ellipse(mouseX, mouseY, 80, 80);
+  s.death();
+  s.update();
+  s.show();
+
+
+  fill(255, 0, 100);
+  rect(food.x, food.y, scl, scl);
+}
+
+function keyPressed() {
+  if (keyCode === UP_ARROW) {
+    s.dir(0, -1);
+  } else if (keyCode === DOWN_ARROW) {
+    s.dir(0, 1);
+  } else if (keyCode === RIGHT_ARROW) {
+    s.dir(1, 0);
+  } else if (keyCode === LEFT_ARROW) {
+    s.dir(-1, 0);
   }
+}
